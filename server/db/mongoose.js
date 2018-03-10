765d8b850db41ee3a86de3cf79652d8669fe1d41
pazpazon <35139232+pazpazon@users.noreply.github.com>
2018-03-10 23:04:32 +0100

    still trying to make heroku connect to db server

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ var connectionString = '';
 
 mongoose.Promise = global.Promise;
 if (process.env.PORT) {
-    connectionString = 'mongodb://admin:P@$$w0rd@ds111319.mlab.com:11319/udemy_nodejs_course_todoapp';
+    connectionString = 'mongodb://dbuser:dbuser12345@ds111319.mlab.com:11319/udemy_nodejs_course_todoapp';
 } else {
     connectionString = 'mongodb://localhost:27017/TodoApp';
 }
