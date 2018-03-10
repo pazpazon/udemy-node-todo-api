1535d12f79d60165e10aa068cae85e914c0b6f61
pazpazon <35139232+pazpazon@users.noreply.github.com>
2018-03-10 22:41:06 +0100

    fixed connection string for mongodb deploy to heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,12 @@
 var mongoose = require('mongoose');
+var connectionString = '';
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://admin:P@$$w0rd@ds111319.mlab.com:11319/udemy_nodejs_course_todoapp' || 'mongodb://localhost:27017/TodoApp');
+if (process.env.PORT) {
+    connectionString = 'mongodb://admin:P@$$w0rd@ds111319.mlab.com:11319/udemy_nodejs_course_todoapp';
+} else {
+    connectionString = 'mongodb://localhost:27017/TodoApp';
+}
+mongoose.connect( connectionString );
 
 module.exports = {mongoose};
