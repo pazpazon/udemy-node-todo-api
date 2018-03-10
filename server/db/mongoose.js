dffde40ef70dc3f304ff1a38a19833e802226c9f
pazpazon <35139232+pazpazon@users.noreply.github.com>
2018-03-10 22:58:33 +0100

    hacking connecion string issues

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -7,6 +7,7 @@ if (process.env.PORT) {
 } else {
     connectionString = 'mongodb://localhost:27017/TodoApp';
 }
+console.log('connectionString set to: ', connectionString);
 mongoose.connect( connectionString );
 
 module.exports = {mongoose};
