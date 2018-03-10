4cd1bc6d152b9c121d7871f2f7d0757916eb9dba
pazpazon <35139232+pazpazon@users.noreply.github.com>
2018-03-10 19:43:31 +0100

    added GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const {ObjectID} = require('mongodb');
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -25,6 +26,21 @@ app.get('/todos', (req, res) => {
         res.send({todos});
     }, (e) => {
         res.status(400).send(e);
+    });
+});
+
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send('invalid id');
+    }
+    Todo.findById(id).then( (todo) => {
+        if (!todo) {
+            return res.status(404).send('id not found');
+        }
+        res.send({todo});
+    }).catch( (e) => {
+        res.status(400).send('server error');
     });
 });
 
