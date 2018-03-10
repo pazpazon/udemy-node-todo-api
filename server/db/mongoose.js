fafb523241caa263d1021fcaf2f83f44e01eddf8
pazpazon <35139232+pazpazon@users.noreply.github.com>
2018-03-10 22:21:56 +0100

    updated db connection data for mlab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://admin:P@$$w0rd@ds111319.mlab.com:11319/udemy_nodejs_course_todoapp' || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
